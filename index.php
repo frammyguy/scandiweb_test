a99926bcf42bcb245c88e318713fa46058659a38
frammguy <vladan4ik771@gmail.com>
2023-03-08 23:16:54 +0200

    page body and begin design

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.min.css">
+	<title>Document</title>
+</head>
+<body>
+
+	<header>
+		<div class="header_text">
+			Product List
+		</div>
+		<nav>
+			<button id="add-product-btn">
+				add
+			</button>
+			<button id="delete-product-btn">
+				mass delete
+			</button>
+		</nav>
+	</header>
+	<section class="main">
+
+	</section>
+	<footer>
+		scandiweb test assignment
+	</footer>
+</body>
+</html>
